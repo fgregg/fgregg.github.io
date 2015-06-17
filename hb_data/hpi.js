e3033cfd5017007739d8315837651cf785a0af58
Forest Gregg <fgregg@uchicago.edu>
2015-06-16 20:43:54 -0500

    hpi

---
+++ hb_data/hpi.js
@@ -0,0 +1,9 @@
+var hpi = {"grouping" : "HPI",
+ "Title" : "Cook County Housing Price Index",
+ "Source" : "Depaul Institute of Housing Studies",
+ "Label" : "HPI",
+ "Start Year" : 2005,
+ "Data Type" : "count",
+ "Point Interval" : "quarter",
+ "Data Raw" : [], 
+ "Data Trend" : [ 156.1,162.54,166.85,170.62,176,176.1,175.5,179.68,177.23,177.33,175.28,169.94,175.89,173.76,166.27,158.41,146.28,138.35,137.36,134.77,130.26,125.94,122.12,121.17,118.02,113.98,111.41,107.61,107.3,108.78,111.89,109.72,111.71,121.58,122.05,126.16,132.34,136.43 ]}
